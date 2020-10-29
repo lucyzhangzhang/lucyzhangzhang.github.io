91b3ff344370b71b8afa59de95e27cc4285434ee
Lucy Zhang <lzsj1995@gmail.com>
2020-10-29 17:17:20 -0400

    initialized files

---
+++ _site.R
@@ -0,0 +1,9 @@
+## packages I want loaded for all pages 
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(stringr)
+})
+
+
+## knitr options I want set as default for all ('global') code chunks
+knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
